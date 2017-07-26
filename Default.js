1de8e5102a160826e6b09b56daa35fb5f6804223
alphons <alphons@moneywise.nl>
2017-07-26 13:44:46 +0200

    Initial release

---
+++ Default.js
@@ -0,0 +1,17 @@
+﻿$(function ()
+{
+	// Example data https://www.w3schools.com/js/js_json_arrays.asp
+	var data = 
+	{
+		"name":"John",
+		"age":30,
+		"cars": 
+		[
+			{ "carid": 1, "name":"Ford", "models":[ "Fiesta", "Focus", "Mustang" ] },
+			{ "carid": 2, "name":"BMW",  "models":[ "320", "X3", "X5" ] },
+			{ "carid": 3, "name":"Fiat", "models":[ "500", "Panda" ] }
+		]
+	};
+	
+	$("#Output").Template("#TemplateItem", data, false);
+});
---
+++ Scripts/moneywise-template.js
@@ -0,0 +1,52 @@
+// moneywise-template.js 3.0
+// 
+// 2016-07-18 AAB - Original
+// 2016-11-23 AAB - Function constructor
+// 2016-11-29 AAB - Function templateid i.p.v. container
+// 2016-11-29 AAB - Nu met script empty en caching werkt nu pas
+// 2016-11-29 AAB - Escaping single quote
+(function ($)
+{
+	$.fn.Template = function (templateid, data, append)
+	{
+		var strHtml = $(templateid).TemplateHtml(data);
+		if (append)
+			return $(this).append(strHtml);
+		else
+			return $(this).html(strHtml);
+	}
+
+	$.fn.TemplateHtml = function (data)
+	{
+		if (!this[0].jscache)
+		{
+			var html = this.html().replace(/[\t\r\n]/g, " ");
+			var js = "var _='';";
+			var intI, intJ = 0;
+			while (intJ < html.length)
+			{
+				intI = html.indexOf("{{", intJ);
+				if (intI < 0)
+					break;
+				js += "_+='" + html.substring(intJ, intI).replace(/\'/g,"\\'") + "';";
+				intJ = intI + 2;
+				direct = (html[intJ] == "=");
+				if (direct)
+					intJ++;
+				intI = html.indexOf("}}", intJ);
+				if (intI < 0)
+					break;
+				if (direct)
+					js += "_+=";
+				js += html.substring(intJ, intI).trim() + ";";
+				intJ = intI + 2;
+			}
+			js += "_+='" + html.substring(intJ).replace(/\'/g,"\\'") + "'; return _;";
+			this[0].jscache = new Function(js);
+			this.empty();
+		}
+		return this[0].jscache.call(data);
+	};
+
+}(jQuery));
+
